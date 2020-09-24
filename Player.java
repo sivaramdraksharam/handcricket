ee31df5a0203c5320f4539d42d784df30af959c6
sivaramdraksharam <35053169+sivaramdraksharam@users.noreply.github.com>
2020-09-24 18:00:23 +0530

    Creating a class called Player
    the class has attributes of a player name, score, is he/she batting, etc.,

---
+++ Player.java
@@ -0,0 +1,15 @@
+package com.gameworld;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Player {
+	//Player properties
+	public int score;     		//Score after each round
+	public boolean batting;  	//batting turn is true if current player is batting
+	public char name;    		//Name of player
+	public int target;   		//target set for chasing player
+	public int run;
+	
+}
+
+   
